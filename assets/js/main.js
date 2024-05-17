22f2249ce023277e80b8c8ee256de426e4989ca0
Matias Arias <matiasarias3103@gmail.com>
2024-05-16 21:28:31 -0400

    [FEAT] add tooltip to the card section and finish the styling of the general landing page

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+function toolTip(card) {
+  new bootstrap.Tooltip(card[0]);
+}
+
+//card1
+const card = $("#card");
+toolTip(card);
+
+//card2
+const card2 = $("#card2");
+toolTip(card2);
+
+//card3
+const card3 = $("#card3");
+toolTip(card3);
+
+//card4
+const card4 = $("#card4");
+toolTip(card4);
